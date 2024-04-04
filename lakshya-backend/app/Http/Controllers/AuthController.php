f5fbabfd9870b73e1e2d1bdfd45de8053d748a83
harshalnix <harshal.mandale@nixcelsoft.com>
2024-04-04 16:28:56 +0530

    encrytion helper and prevent back history middleware implemented

--- lakshya-backend/app/Http/Controllers/AuthController.php
+++ lakshya-backend/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Helpers\EncDecHelper;
 
 
 class AuthController extends Controller
@@ -11,34 +12,24 @@ class AuthController extends Controller
     //
     public function login(Request $request)
     {
-        
-        // $data = [
-        //     'message' => 'Welcome!',
-        //     'status' => 'success'
-        // ];
-    
-         //return response()->json($request);
+        //find the user from email id
         $user = User::where('u_email',$request->email)->first();
         
-        //check if user is found
+        //check email is invalid return invalid error msg
         if (!$user) {
             return response()->json(['error' => 'Invalid email. Please enter a valid email.'], 400);
         }
-       // return response()->json($user);
-       //$password =$request->password;
-       
-       //encrypt the password
-       // $encrypted_pass = EncryptionDecryptionHelper::encryptData($password);
-     
+       //return response()->json($request);
+        //enc the pass recived from the request
+        $encPass = EncDecHelper::encryptData($request->password);
        
         //if user exists validate password and redirect to respective page
-        if (strcmp($user->u_password, $request->password) === 0) {
+        if (strcmp($user->u_password, $encPass) === 0) {
             return response()->json(['message' => 'Admin logged in successfully'], 200);
         }else {
             // Password does not match, return error response
             return response()->json(['error' => 'Invalid password. Please enter a valid password.'], 400);
         }
-
 
     }
 }
